2d43af6d72c3aad5cdb8f5ca0f28677016daa2e7
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-09-26 17:53:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_Practice_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("whoops, I made the first two private. Ill ask you how to make them public in class");
+        }
+    }
+}
